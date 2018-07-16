c8188826f341a0a79f66a96d5af09b666cd96537
ryan-limabean <ryan@limabean.co.za>
2018-07-16 14:14:02 +0200

    vue commits

--- src/assets/js/components/MenuCart.vue
+++ src/assets/js/components/MenuCart.vue
@@ -1,30 +1,44 @@
 <template>
     
     <div> 
-        <h1>IMA CART</h1>
-
-        <p v-for="item in items">
+      
+        <h1>CART</h1>
+        <p v-for="(item,index) in items" :key="index">
             {{ item.name }}: {{ item.qty }}
         </p>
-    </div>
+        
+   </div>
 </template>
+   
 
 
 <script>
+var p1counter=0;
+var p2counter=0;
+var p3counter=0;
+var p4counter=0;
 export default {
     name: "menu-cart",
-
+   
     data: () => {
+         
         return {
+              
+
             items: [
-                {name:"Test", qty:3},
-                {name:"Test", qty:3},
-                {name:"Test", qty:3},
-                {name:"Test", qty:3},
-            ]
+                {name:"Sweet nuts", qty:p1counter},
+                {name:"Pasta sauces", qty:p2counter},
+                {name:"Snacks", qty:p3counter},
+                {name:"Hampers", qty:p4counter}
+            ],
+
+
+            }
+
         }
+         
     }
-}
+
 </script>
 
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import Vue from 'vue';
 import whatInput from 'what-input';
 
-window.$ = $;\
+window.$ = $;
 
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
@@ -18,5 +18,8 @@ var app = new Vue({
     'el': '#prod',
     components: {
         MenuCart
-    }    
+    }
+   
 })
+
+      
